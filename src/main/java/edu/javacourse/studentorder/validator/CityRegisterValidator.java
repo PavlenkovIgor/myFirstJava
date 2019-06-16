4a994f38882b3af32c2f79563e0a0c5d385691ba
igor <pavlenkov.igor@bk.ru>
2019-06-16 22:36:32 +0300

    16.06.2019

--- src/main/java/edu/javacourse/studentorder/validator/CityRegisterValidator.java
+++ src/main/java/edu/javacourse/studentorder/validator/CityRegisterValidator.java
@@ -1,30 +1,45 @@
 package edu.javacourse.studentorder.validator;
 
-import edu.javacourse.studentorder.domain.AnswerCityRegister;
-import edu.javacourse.studentorder.domain.CityRegisterCheckerResponse;
+
+import edu.javacourse.studentorder.domain.Child;
+import edu.javacourse.studentorder.domain.Person;
+import edu.javacourse.studentorder.domain.register.AnswerCityRegister;
+import edu.javacourse.studentorder.domain.register.AnswerCityRegisterItem;
+import edu.javacourse.studentorder.domain.register.CityRegisterResponse;
 import edu.javacourse.studentorder.domain.StudentOrder;
+import edu.javacourse.studentorder.exception.CityRegisterException;
+import edu.javacourse.studentorder.validator.register.CityRegisterChecker;
+import edu.javacourse.studentorder.validator.register.FakeCityRegisterChecker;
 
-public class CityRegisterValidator {
+import java.util.List;
 
-   public String hostName;
-    String login;
-    String password;
-
+public class CityRegisterValidator
+{
     private CityRegisterChecker personChecker;
 
-    public CityRegisterValidator(){
+    public CityRegisterValidator() {
         personChecker = new FakeCityRegisterChecker();
     }
 
-
-        public AnswerCityRegister checkCityRegister(StudentOrder so){
-
-        CityRegisterCheckerResponse hans = personChecker.checkPerson(so.getHusband());
-        CityRegisterCheckerResponse wans = personChecker.checkPerson(so.getWife());
-        CityRegisterCheckerResponse cans = personChecker.checkPerson(so.getChild());
-
+    public AnswerCityRegister checkCityRegister(StudentOrder so) {
         AnswerCityRegister ans = new AnswerCityRegister();
 
+        ans.addItem(checkPerson(so.getHusband()));
+        ans.addItem(checkPerson(so.getWife()));
+        for (Child child : so.getChildren()) {
+            ans.addItem(checkPerson(child));
+        }
+
         return ans;
+    }
+
+    private AnswerCityRegisterItem checkPerson(Person person) {
+        try {
+            CityRegisterResponse cans = personChecker.checkPerson(person);
+        } catch (CityRegisterException ex) {
+            ex.printStackTrace(System.out);
+        }
+
+        return null;
     }
 }
